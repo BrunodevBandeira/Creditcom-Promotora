ad48a4b16cabb41457cf66d8457f3a16922426cc
Bruno Bandeira <brunodev29ab@gmail.com>
2021-12-29 02:21:48 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $(".services").on("mouseover", function() {
+        $(".box1").addClass("efeito")
+    }).on("mouseout", function() {
+        $(".box1").removeClass("efeito")
+    })
+
+})
